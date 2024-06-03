0cfe157c6c87de699ae8355838a48d7f1a090966
South Korea Shuan <161431602+i-am-shuan@users.noreply.github.com>
2024-06-03 20:39:12 +0900

    Update disconnect_lambda.py

--- lambda/disconnect_lambda.py
+++ lambda/disconnect_lambda.py
@@ -1,7 +1,25 @@
 import json
+import boto3
+import os
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Success"
-    }
+    connection_id = event['requestContext']['connectionId']
+    client = boto3.client('apigatewaymanagementapi', endpoint_url=os.environ['api_endpoint'], region_name=os.environ['region'])
+
+    try:
+        # 연결 종료
+        client.delete_connection(ConnectionId=connection_id)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Disconnected successfully')
+        }
+    except client.exceptions.GoneException:
+        return {
+            'statusCode': 410,
+            'body': json.dumps('Connection already closed')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Failed to disconnect: {str(e)}')
+        }
